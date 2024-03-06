5560bbf86e5d5082907213bd0a7d456dc067b27c
Youp Hendriks <youp_hendriks@hotmail.com>
2024-03-06 13:35:13 +0100

    Fixed formatting after pulling

--- org.bridgedb/src/test/java/org/bridgedb/DataSourcePatternsTest.java
+++ org.bridgedb/src/test/java/org/bridgedb/DataSourcePatternsTest.java
@@ -1,45 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< ours
-<<<<<<< HEAD
-<<<<<<< HEAD
 /*Copyright (c) 2024 Youp Hendriks <youp_hendriks@hotmail.com>
-=======
-=======
->>>>>>> ef38fbc2 (A bit more header clean up)
-=======
->>>>>>> f08d154a (Added name to citation file)
-/*
- *BridgeDb,
- *An abstraction layer for identifier mapping services, both local and online.
- *Copyright (c) 2024 Youp Hendriks<youp_hendriks@hotmail.com>
- *Copyright (c) 2024 OpenPhacts
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> e9b78be9 (Added name to citation file)
-=======
-=======
-/*Copyright (c) 2024 Youp Hendriks <youp_hendriks@hotmail.com>
->>>>>>> 7a8142ea (A bit more header clean up)
->>>>>>> ef38fbc2 (A bit more header clean up)
-=======
-/*
- *Copyright (c) 2024 Youp Hendriks<youp_hendriks@hotmail.com>
->>>>>>> theirs
-=======
->>>>>>> f08d154a (Added name to citation file)
- *
- *Licensed under the Apache License, Version 2.0 (the "License");
- *you may not use this file except in compliance with the License.
- *You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- *Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
- *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *See the License for the specific language governing permissions and limitations under the License.
- */
-<<<<<<< HEAD
-=======
+*
+*Licensed under the Apache License, Version 2.0 (the "License");
+*you may not use this file except in compliance with the License.
+*You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+*
+*Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+*WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*See the License for the specific language governing permissions and limitations under the License.
+*/
 
->>>>>>> f08d154a (Added name to citation file)
 package org.bridgedb;
 
 import static org.junit.jupiter.api.Assertions.*;
